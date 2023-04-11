e3d3d002b6f9d2d4c73f19d92d8b3863e05b7f9a
SUP2Ak <31973315+SUP2Ak@users.noreply.github.com>
2023-04-12 01:33:01 +0200

    add: documentation config files / chore (resource.name)

--- config/client.lua
+++ config/client.lua
@@ -1,5 +1,12 @@
+---@class targetProps
+---@field name string
+---@field icon? string
+---@field label string
+---@field groups? table
+
+---@type {[string]: targetProps}
 Config.target = {
-    ['reseller_1'] = {
+    ['reseller_1'] = { -- key from Config.coords
         name = 'ox:reseller1', -- change name for every reseller you make
         icon = "fa-solid fa-person",
         label = 'Interagir',
@@ -7,53 +14,82 @@ Config.target = {
     }
 }
 
+---@class pedProps
+---@field model string|hash
+---@field data? {blockevent?: boolean-true, freeze?: boolean-true, godmode?: boolean-true, variation?: integer-0}|nil-false  ---@default {blockevent = true, freeze = true, godmode = true, variation = 0}
+---@field weapon? {model: string|hash, ammo?: integer-0, visible?: boolean|true}|nil-false
+
+---@type {[string]: pedProps}
 Config.ped = {
-    ['reseller_1'] = {
+    ['reseller_1'] = { -- key from Config.coords
         model = `s_m_m_security_01`,
-        --data = {}, -- or nil
-        --weapon = {} -- or nil
+        --data = nil, -- or {}
+        --weapon = nil -- or {}
     }
 }
 
+---@class categorieProps
+---@field [string]: string
 Config.cat = {
     ['rifle'] = "Fusil d'assaut",
     ['melee'] = "Arme de mélée",
     ['pistol'] = "Arme de poing"
 }
 
+---@class mainProps
+---@field title string
+---@field description? string
+---@field menu categorieProps -- key from Config.cat
+---@field icon? string
+
+---@class weaponProps
+---@field label string
+---@field image string
+
+---@class subProps
+---@field [weapon_name: string]: weaponProps
+
+---@class menuProps
+---@field title string
+---@field main mainProps
+---@field sub {[menu]: subProps}
+
+---@type {[string]: menuProps}
 Config.menu = {
-    ['reseller_1'] = {
+    ['reseller_1'] = { -- key from Config.coords
         title = 'Jo le bargo',
-        ['main'] = {
+        main = {
             {
                 title = 'Melée',
                 description = nil,
-                menu = 'melee',
+                menu = 'melee', -- key from Config.cat
                 icon = 'utensils'
             },
             {
                 title = 'Pistolet',
                 description = nil,
-                menu = 'pistol',
+                menu = 'pistol', -- key from Config.cat
                 icon = 'gun'
             },
             {
                 title = 'Fusil',
                 description = nil,
-                menu = 'rifle',
+                menu = 'rifle', -- key from Config.cat
                 icon = 'person-rifle'
             },
         },
 
-        ['sub'] = {
-            ['melee'] = {
+        sub = {
+            ---@type weaponProps
+            ['melee'] = { -- key from main.menu
                 ['weapon_knife'] = {
                     label = "Couteau",
                     image = "https://raw.githubusercontent.com/overextended/ox_inventory/main/web/images/WEAPON_KNIFE.png",
                 }
             },
     
-            ['pistol'] = {
+            ---@type weaponProps
+            ['pistol'] = { -- key from main.menu
                 ['weapon_pistol'] = {
                     label = "Pistolet",
                     image = "https://raw.githubusercontent.com/overextended/ox_inventory/main/web/images/WEAPON_PISTOL.png",
@@ -64,7 +100,8 @@ Config.menu = {
                 }
             },
     
-            ['rifle'] = {
+            ---@type weaponProps
+            ['rifle'] = { -- key from main.menu
                 ['weapon_assaultrifle'] = {
                     label = "Ak-47",
                     image = "https://raw.githubusercontent.com/overextended/ox_inventory/main/web/images/WEAPON_ASSAULTRIFLE.png"
--- config/shared.lua
+++ config/shared.lua
@@ -1,14 +1,27 @@
-Config.coords = {
+---@type {[string]: vec4}
+Config.coords = { -- init key with coords for every Config table
     ['reseller_1'] = vec4(118.22733306885, -1328.2004394531, 29.356643676758-1, 298.95544433594),
 }
 
+---@class priceProps
+---@field weapon_name number
+
+---@type {[string]: priceProps}
 Config.price = {
-    ['reseller_1'] = {['weapon_pistol'] = 500, ['weapon_assaultrifle'] = 5000, ['weapon_knife'] = 10, ['weapon_appistol'] = 1250}
+    ['reseller_1'] = { -- key from Config.coords
+        ['weapon_pistol'] = 500, ['weapon_assaultrifle'] = 5000, ['weapon_knife'] = 10, ['weapon_appistol'] = 1250
+    }
 }
 
+---@class moneyProps
+---@field value string
+---@field label string
+
+---@type {[string]: moneyProps}
 Config.money = {
-    ['reseller_1'] = {
+    ['reseller_1'] = { -- key from Config.coords
         {value = 'money', label = 'Cash'}, -- index 1 set per default
-        {value = 'black_money', label = 'Argent sale'}
+        {value = 'black_money', label = 'Argent sale'},
+        --{value = 'bank', label = 'Banque'}
     }
 }
